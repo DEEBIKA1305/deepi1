7530d7351df0fbaeb91bc95fe4078a73f22c0730
DEEBIKA1305 <tdeepika4eee@gmail.com>
2017-06-06 15:55:25 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main{
+int alpha;
+printf("Enter any alphabet:");
+scanf("%d",&alpha);
+if(alpha==a||alpha==e||alpha==i||alpha==o||alpha==u){
+printf("The alphabet is a vowel");
+}
+else
+{
+printf("The alphabet is a consonant");
+}
+return 0;
+}
